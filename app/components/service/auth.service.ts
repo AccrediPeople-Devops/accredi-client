c55c551407519afbe95edc6587dd3c7a26db5ebc
Vrushab Kudchi <kudchivrushab@gmail.com>
2025-05-11 17:43:55 +0530

    Enhance axiosInstance and AuthService for token management
    - Updated axiosInstance to use a configurable base URL and added interceptors for handling token refresh on 401 responses.
    - Modified AuthService to store both access and refresh tokens, and implemented a method to generate a new access token using the refresh token.
    - Improved isAuthenticated method to verify user authentication status via API call.

--- app/components/service/auth.service.ts
+++ app/components/service/auth.service.ts
@@ -10,7 +10,8 @@ class AuthService {
 
       // Only set token if we're on the client side
       if (typeof window !== "undefined") {
-        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("token", response.data.token.accessToken);
+        localStorage.setItem("refreshToken", response.data.token.refreshToken);
       }
 
       return response.data;
@@ -31,14 +32,38 @@ class AuthService {
   logout() {
     if (typeof window !== "undefined") {
       localStorage.removeItem("token");
+      localStorage.removeItem("refreshToken");
     }
   }
 
-  isAuthenticated() {
-    if (typeof window !== "undefined") {
-      return !!localStorage.getItem("token");
+  async isAuthenticated() {
+    try {
+      const response = await axiosInstance.get("/auth/v1", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.status === 200) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      throw error;
     }
-    return false;
+  }
+
+  async generateTokenByRefreshToken() {
+    try {
+      const response = await axiosInstance.post("/auth/v1/refresh-token", {
+        refreshToken: localStorage.getItem("refreshToken"),
+      });
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+      }
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
