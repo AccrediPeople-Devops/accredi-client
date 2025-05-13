f379144475b85f5757f2099412a8645b9077b3b9
Vrushab K <kudchivrushab@gmail.com>
2025-05-13 13:45:39 +0000

    Add Dockerfile and update configuration for port settings

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["placehold.co","via.placeholder.com", "courses.daylink.in", "148.135.137.229"],
   },
+  server: {
+    port: 8080
+  }
 };
 
 module.exports = nextConfig;
